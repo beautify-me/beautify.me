0d38f376fbd1a0c54f77fd527fa6efc3d85b9580
Hennig <Markus@131.159.199.163>
2013-06-17 12:50:11 +0200

    added some requirements in Models/User

--- app/models/User.java
+++ app/models/User.java
@@ -7,14 +7,19 @@ import java.util.Set;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 
+import play.data.validation.*;
+
 import play.db.jpa.Model;
+
 @Entity
 public class User extends Model {
 	
-	public String name;
-	public String lastName;
-	public String userName;
-	public String email;
+	@Required public String name;
+	@Required public String lastName;
+	@Required @MinSize(6) public String userName;
+	@Required @Email public String email;
+	@Range(min=12, max=120) public Integer age;
+	
 	public String avatarUrl;
 	@ElementCollection
 	public Map<String, Accessory> myAccesories;
