437177f391b7aa70386e1ac0b31c74058d43b164
magic-muschrum <worldwidetobi@gmx.de>
2013-07-11 18:09:23 +0200

    changed bootstrap to put pics onto the database

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -56,7 +56,7 @@ public class Bootstrap extends Job {
 
 	private void createAccessories() {
 		// Check if the database is empty
-		if (Accessory.count() == 0) {
+//		if (Accessory.count() == 0) {
 			List<Accessory> allItems = Accessory.findAll();
 
 			for (Accessory a : allItems) {
@@ -64,7 +64,7 @@ public class Bootstrap extends Job {
 						+ a.provider.name.toLowerCase()
 						+ ".png|image/png' WHERE id=" + a.getId());
 
-			}
+//			}
 		}
 	}
 
